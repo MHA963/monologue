a020152c93729d96476ce437a9c041cd2d06f3dd
MHA <98241106+MHA963@users.noreply.github.com>
2026-04-06 12:27:38 +0200

    vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,11 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig} from 'vite';
 
-export default defineConfig(() => {
+const projectName = path.basename(__dirname).toLowerCase();
+
+export default defineConfig(({command}) => {
   return {
+    base: command === 'build' ? `/${projectName}/` : '/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
